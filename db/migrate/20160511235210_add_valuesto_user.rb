c8fa0931ca7dea85faa68fa94659e80a196343da
Sophie Tan <tansipei@gmail.com>
2016-05-16 12:54:22 -0400

    updated hootie app

---
+++ db/migrate/20160511235210_add_valuesto_user.rb
@@ -0,0 +1,9 @@
+class AddValuestoUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :username, :string
+    add_column :users, :bio, :text
+    add_column :users, :location, :string
+
+  end
+end
